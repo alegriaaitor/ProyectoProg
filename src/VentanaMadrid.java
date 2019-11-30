439dbba289920941750bd56305a871826e20c143
PC-John <PC-John@DESKTOP-P8PC4M9>
2019-11-30 13:40:45 +0100

    ventana madrid

--- src/VentanaMadrid.java
+++ src/VentanaMadrid.java
@@ -4,10 +4,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class VentanaMadrid extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -30,11 +38,86 @@ public class VentanaMadrid extends JFrame {
 	 */
 	public VentanaMadrid() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 624, 427);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblMadrid = new JLabel("MADRID");
+		lblMadrid.setBounds(28, 13, 56, 16);
+		contentPane.add(lblMadrid);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(12, 77, 56, 16);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(12, 161, 56, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(12, 245, 56, 16);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setBounds(12, 330, 56, 16);
+		contentPane.add(lblNewLabel_3);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setBounds(110, 44, 116, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		textField_1.setBounds(110, 134, 116, 22);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		textField_2.setBounds(110, 214, 116, 22);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setEditable(false);
+		textField_3.setBounds(110, 299, 116, 22);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JButton btnVerHotel = new JButton("VER HOTEL");
+		btnVerHotel.setBounds(404, 73, 97, 25);
+		contentPane.add(btnVerHotel);
+		
+		JButton btnVerHotel_1 = new JButton("VER HOTEL");
+		btnVerHotel_1.setBounds(404, 161, 97, 25);
+		contentPane.add(btnVerHotel_1);
+		
+		JButton btnVerHotel_2 = new JButton("VER HOTEL");
+		btnVerHotel_2.setBounds(404, 254, 97, 25);
+		contentPane.add(btnVerHotel_2);
+		
+		JButton btnVerHotel_3 = new JButton("VER HOTEL");
+		btnVerHotel_3.setBounds(404, 342, 97, 25);
+		contentPane.add(btnVerHotel_3);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(106, 76, 286, 55);
+		contentPane.add(textArea);
+		
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setBounds(110, 162, 282, 49);
+		contentPane.add(textArea_1);
+		
+		JTextArea textArea_2 = new JTextArea();
+		textArea_2.setBounds(110, 242, 282, 49);
+		contentPane.add(textArea_2);
+		
+		JTextArea textArea_3 = new JTextArea();
+		textArea_3.setBounds(110, 327, 282, 53);
+		contentPane.add(textArea_3);
 	}
-
 }
